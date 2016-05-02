8f0ffc4cc4c8bf09fd77e9ca8720e8137eb8c8e7
Joy Wangari <warugujoy@gmail.com>
2016-05-02 09:18:35 +0300

    Put validation methods for username and email

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,8 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField, BooleanField, HiddenField
-from wtforms.validators import Required
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 class LogInForm(Form):
 	email = StringField('Email', validators=[Required(), Email(), Length(1, 64)])
@@ -9,8 +11,21 @@ class LogInForm(Form):
 	submit = SubmitField('Log In')
 
 class SignUpForm(Form):
-	email = StringField('Email', validators=[Required(), Email(), Length(1, 64),])
-    username = StringField('Username', validators=[Required(), Length(1, 64)])
-    password = HiddenField('Password', validators=[Required(), EqualTo('password2', message="Your passwords do no match. Please re-enter")])
+	email = StringField('Email', validators=[Required(), Email(), 
+											 Length(1, 64)])
+    username = StringField('Username', validators=[
+    	 Required(), Length(1, 64)], Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+											'Usernames must have only letters, '
+											'numbers, dots or underscores'))
+    password = HiddenField('Password', validators=[
+    	Required(), EqualTo('password2', message="Your passwords do no match. Please re-enter")])
     password2 = HiddenField('Confirm password', validators=[Required()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self, field):
+    	if User.query.fiter_by(email=field.data).first():
+    		raise ValidationError('Email already registered')
+
+    def validate_username(self, field):
+    	if User.query.fiter_by(username=field.data).first():
+    		raise ValidationError('Username already in use')
