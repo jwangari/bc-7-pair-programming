7ab6bacc4084fdffe7e10b2adc37530591498886
Joy Wangari <warugujoy@gmail.com>
2016-05-02 08:16:40 +0300

    Implement logout function

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,13 +5,21 @@ from . import auth
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+	'''
+		This view function creates a LoginForm object and uses it like a simple form.
+			-When request is of type GET, the view function renders template which
+				displays the form
+			-When request is of type POST, the Flask WTF's validate_on_submit()
+				validates the form variables and then attempts to log the user in
+	'''
 	form = LogInForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(email=form.email.data).first()
 		if user is not None and user.verify_password(form.password.data):
 			login_user(user, form.keep_on.data)
 			return redirect(url_for('main.index'))
-	return render_template('auth/login.html')
+		flash('Invalid username and password combination.')
+	return render_template('auth/login.html', form=form)
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -25,6 +33,16 @@ def signup():
 		return redirect(url_for('main.index'))
 	return render_template('signin.html')
 
-#@auth.route('/logout')
-
+@auth.route('/logout')
+@login_required
+def logout():
+	'''
+		The logout() function calls the Flask Login function logout_user() that
+			removes and resets user session. 
+		The logout is completed with a flash message that confirms the action
+			and redirect to the home page.
+	'''
+	logout_user()
+	flash('You have been logged out.')
+	return redirect(url_for('main.index'))
 
