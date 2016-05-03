21f6fab54de3c1ec0a5f88a6a44ffa974e7c0965
Joy Wangari <warugujoy@gmail.com>
2016-05-03 09:49:39 +0300

    Fix errors that occured

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from config import config
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 #Initialization of the Flask-Login
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -9,14 +15,20 @@ def create_app(config_name):
 	'''
 		Is a factory function which allows specification of 
 		cofigurations before the app is created.
+		It takes as an argument the name of a configuration 
+		to use for the application.
 		
 	'''
 	app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
+	app.config.from_object(config[config_name])
+	config[config_name].init_app(app)
+	login_manager.init_app(app)
+	bootstrap.init_app(app)
+	db.init_app(app)
 
-    login_manager.init_app(app)
-
+	from main import main as main_blueprint
 	from auth import auth as auth_blueprint
-	app.register_blueprint(auth_blueprint, url_prefix='auth')
+
+	app.register_blueprint(main_blueprint)
+	app.register_blueprint(auth_blueprint)
 	return app
