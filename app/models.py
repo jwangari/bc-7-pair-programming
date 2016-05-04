69232c0f29c17620a463d8b43057d175951cace1
Joy Wangari <warugujoy@gmail.com>
2016-05-04 22:20:08 +0300

    Add a new table for sessions

--- app/models.py
+++ app/models.py
@@ -2,6 +2,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
 from app import db, login_manager
 
+registrations = db.Table('registrations',
+	db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+	db.Column('session_id', db.Integer, db.ForeignKey('sessions.id')))
+
 class User(UserMixin, db.Model):
 	__tablename__ = 'users'
 
@@ -9,22 +13,30 @@ class User(UserMixin, db.Model):
 	username = db.Column(db.String(64), unique=True, index=True)
 	email = db.Column(db.String(64), unique=True, index=True)
 	password_hash = db.Column(db.String(128))
+	sessions = db.relationship('ProgramSession',
+								secondary=registrations,
+								backref=db.backref('users', lazy='dynamic'),
+								lazy='dynamic')
 
 	@property 
 	def password(self):
 		'''
+			This function raises an error if you try to read the password value.
 		'''
 		raise AttributeError('password cannot be read (write-only attribute)')
 
 	@password.setter
 	def password(self, password):
 		'''
+			Generates an password hash for a given password.
 		'''
 		self.password_hash = generate_password_hash(password)
 
 
 	def verify_password(self, password):
 		'''
+			Compares the hash stored in the database with the
+			password value given by user.
 		'''
 		return check_password_hash(self.password_hash, password)
 
@@ -36,12 +48,13 @@ class User(UserMixin, db.Model):
 		'''
 		return User.query.get(int(user_id))
 
-# class ProgramSession():
-# 	__tablename__ = 'session'
+class ProgramSession(db.Model):
+	__tablename__ = 'sessions'
 
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	session_name = db.Column(db.String(128), unique=True, index=True)
-#     session_URL = db.Column(db.String(128))
-#     program_lang = db.Column(db.String(128))
+	id = db.Column(db.Integer, primary_key=True)
+	session_name = db.Column(db.String(128), unique=True, index=True)
+	session_url = db.Column(db.String(128))
+	program_lang = db.Column(db.String(128))
+
 
 	
