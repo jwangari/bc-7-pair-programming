f8482e4c9baf81bd810528a08e1fb182951a4732
Joy Wangari <warugujoy@gmail.com>
2016-05-10 12:23:45 +0300

    Changed config file

--- config.py
+++ config.py
@@ -19,8 +19,6 @@ class DevelopmentConfig(Config):
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = 'warugujoy@gmail.com'
-	MAIL_PASSWORD = 'spdwndzzvvgwtucr'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
